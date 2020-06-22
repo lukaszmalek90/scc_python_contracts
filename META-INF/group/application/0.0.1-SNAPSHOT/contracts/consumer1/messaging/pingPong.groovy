d4f8c60a028e832a5f5e69ddf5264085b51f7085
Marcin Grzejszczak <marcin@grzejszczak.pl>
2020-06-22 16:56:28 +0200

    Update pingPong.groovy

--- META-INF/group/application/0.0.1-SNAPSHOT/contracts/consumer1/messaging/pingPong.groovy
+++ META-INF/group/application/0.0.1-SNAPSHOT/contracts/consumer1/messaging/pingPong.groovy
@@ -6,10 +6,7 @@ Contract.make {
     description 'Send a pong message in response to a ping message'
     label 'ping_pong'
     input {
-        messageFrom('input')
-        messageBody([
-            message: 'ping'
-        ])
+        triggeredBy('triggerMessage("ping_pong")')
     }
     outputMessage {
         sentTo('output')
