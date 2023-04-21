40fce5ec4fc40ba2c04a998908feefa88d5e2d65
lukaszmalekharman <131248654+lukaszmalekharman@users.noreply.github.com>
2023-04-21 12:56:25 +0200

    Update pingPong.groovy

--- META-INF/group/application/0.0.1-SNAPSHOT/contracts/consumer1/messaging/pingPong.groovy
+++ META-INF/group/application/0.0.1-SNAPSHOT/contracts/consumer1/messaging/pingPong.groovy
@@ -1,32 +1,32 @@
-package consumer1
+// package consumer1
 
-import org.springframework.cloud.contract.spec.Contract
+// import org.springframework.cloud.contract.spec.Contract
 
-Contract.make {
-    description 'Send a pong message in response to a ping message'
-    label 'ping_pong'
-    input {
-        // You have to provide the `triggerMessage` method with the `label`
-        // as a String parameter of the method
-        triggeredBy('triggerMessage("ping_pong")')
-    }
-    outputMessage {
-        sentTo('output')
-        body([
-            message: 'pong'
-        ])
-    }
-    metadata(
-        [amqp:
-         [
-           outputMessage: [
-               connectToBroker: [
-                   declareQueueWithName: "queue"
-               ],
-                messageProperties: [
-                    receivedRoutingKey: '#'
-                ]
-           ]
-         ]
-        ])
-}
+// Contract.make {
+//     description 'Send a pong message in response to a ping message'
+//     label 'ping_pong'
+//     input {
+//         // You have to provide the `triggerMessage` method with the `label`
+//         // as a String parameter of the method
+//         triggeredBy('triggerMessage("ping_pong")')
+//     }
+//     outputMessage {
+//         sentTo('output')
+//         body([
+//             message: 'pong'
+//         ])
+//     }
+//     metadata(
+//         [amqp:
+//          [
+//            outputMessage: [
+//                connectToBroker: [
+//                    declareQueueWithName: "queue"
+//                ],
+//                 messageProperties: [
+//                     receivedRoutingKey: '#'
+//                 ]
+//            ]
+//          ]
+//         ])
+// }
